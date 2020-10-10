5d8ada0562cbef19552f983e38be4e0e169f726c
JT <jt@jtonto.com>
2020-10-10 14:50:16 -0500

    created a hello generator

--- 1Program.py
+++ 1Program.py
@@ -1,6 +1,35 @@
 numberOfTimes= 5
+anyName= 'Chuck'
+'''
+#Test 1
+if True== True:
+    print('True is True')
+else:
+    print('This is not true')
+#Test 2
+if True== 'True':
+    print('True is True')
+else:
+    print('This is not true')
+#Test 3
+if 1== 1:
+    print('True is True')
+else:
+    print('This is not true')
+#Test 4
+if 1+1 == 2:
+    print('True is True')
+else:
+    print('This is not true')
+'''
+flag= True
 
-print(f'Hello this is my {numberOfTimes}th program')
-
-print('hooray')
-print('here goes another line')
+while flag== True:
+    anyName= input('What is your name? (Type "stop" to stop) ')
+    if anyName.lower()!= "stop":
+        print(f'hooray {anyName}')
+    else:
+        flag= False
+        print('Have a nice day.')
+    
+    
