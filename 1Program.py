6e5ac08ab9daecfe47147173c792c7c54f6d050d
smikho <smikho4@gmail.com>
2020-10-10 14:57:01 -0500

    hi

--- 1Program.py
+++ 1Program.py
@@ -1,6 +1,11 @@
 numberOfTimes= 5
-
+anyNAme = 'Jt'
 print(f'Hello this is my {numberOfTimes}th program')
 
-print('hooray')
+print('hooray' + anyNAme)
 print('here goes another line')
+
+while True:
+    anyNAme= input('What is your name?')
+    print(f'hooray {anyNAme}')
+    
